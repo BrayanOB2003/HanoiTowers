796fa2e452c724f3db6f8a957da8336f132ccaae
OrtizBrayan <brayanortizbastidas@gmail.com>
2021-04-17 06:12:20 -0500

    add iterator

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,37 +1,42 @@
 package ui;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 	
-	public static void hanoi(int n, int c, int origin, int aux, int destiny) {
+	public static void hanoi(int n, int origin, int aux, int destiny) {
 		System.out.println(origin + " " + aux + " " + destiny);
-		int cont = c;
 		if(n==1) {
 			System.out.println((origin-1) + " " + aux + " " + (destiny+1));
 		} else {
-			hanoi(n - 1, cont++, origin - 1, aux, destiny + 1);
+			hanoi(n - 1, origin - 1, aux, destiny + 1);
 			System.out.println(origin + " " + aux + " " + destiny);
-			hanoi(n-1,cont++,origin-1 , aux+1, destiny);
-			
+			hanoi(n-1,origin-1 , aux+1, destiny);
 		}
 	}
 	
 	public static void iterate(int x, int n) {
 		
 		if(x < n) {
-			System.out.println(n+" 0 0");
-			hanoi(n, 1, n, 0, 0);
+			hanoi(n, n, 0, 0);
 			System.out.println();
 			iterate(x+1, n);
 		}
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		//System.out.println(n+" 0 0");
-		hanoi(n, 0, n, 0, 0);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(bf.readLine());
+		int n = 0;
+		for(int i = 0; i < size; i++) {
+			n = Integer.parseInt(bf.readLine());
+			hanoi(n, n, 0, 0);
+			System.out.println("");
+		}
+		
+		
 	}
 
 }
